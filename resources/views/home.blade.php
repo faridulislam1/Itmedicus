ce9d117c2405ca5f5154185c0b55c23cac1e4266
faridul <ffaridulislam41@gmail.com>
2023-03-22 13:34:04 +0600

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,15 @@
+@extends ('layouts.dashboard')
+
+@section('content')
+    <div class="container">
+        <div class="row mt-5">
+            <div class="col-md-12">
+                <div class="card">
+                    <div class="card-header bg-primary">
+                        <h1 class="text-center">Welcome to my Dashboard</h1>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
